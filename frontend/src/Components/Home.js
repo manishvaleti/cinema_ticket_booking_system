f8ab775ec5714089961fea25881bd1abeef2f913
manishvaleti <manishvaleti@gmail.com>
2024-02-13 14:29:42 -0500

    second commit

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '/Users/manishvaleti/Desktop/try2/frontend/src/logo.svg'; // Assuming you have a logo file named logo.svg in the same directory
+//import logo from 'frontend/src/logo.svg'; // Assuming you have a logo file named logo.svg in the same directory
 
 function Home() {
   return (
     <div className="Home">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/Components/Home.js</code> and save to reload.
         </p>
