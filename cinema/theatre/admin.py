845ab8277c540cf05aa359332febd1e148b25d4c
manishvaleti <manishvaleti@gmail.com>
2024-02-20 15:01:25 -0500

    try commit

--- cinema/theatre/admin.py
+++ cinema/theatre/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Movie, User
+from .models import *
 
 admin.site.register(Movie)
 # Register your models here.
 admin.site.register(User)
+admin.site.register(PromoCode)
