76aa0e73dc4073d08ea38fcc8d8a2d3ddf2c7196
Michael Hooper <michaelhooper44@gmail.com>
2024-02-21 21:55:42 -0500

    styled Home, movieInd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import Registration from './Components/Registration';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Update import statement
 import UserList from './Components/UserList';
 import Home from './Components/Home'; // Assuming you have a Home component for the homepage
-import MovieDetails from './Components/movieInd'
+import MovieDetails from './Components/movieInd';
+import Login from './Components/Login';
+
 function App() {
   return (
     <Router>
       <Routes> 
         <Route path="/" element={<Home />} /> {/* Use element prop to specify the component */}
         <Route path="/users" element={<UserList />} />
-        <Route path="/Register" element={<Registration />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Registration />} />
         <Route path="/movies/:movieId/" element={<MovieDetails />} />
         {/* Add more routes for other pages */}
